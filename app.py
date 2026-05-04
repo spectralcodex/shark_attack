c6343c55fffe7b743980ff693a21a16236a47699
Steph Freeman <stephen.adarkwah@nice.org.uk>
2026-05-04 12:21:31 +0100

    uv extensions added

--- app.py
+++ app.py
@@ -5,31 +5,53 @@ import os
 
 app = Flask(__name__)
 
+
 # index page
 @app.route("/")
 def main_page():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # product details page
-@app.route("/productdetails", methods=['POST'])
+@app.route("/productdetails", methods=["POST"])
 def product_page():
     try:
-        productID = request.form.get('productID')
+        productID = request.form.get("productID")
         print(productID)
 
         # Server connections
-        server = 'tcp:techchipsdbserver1.database.windows.net'
-        database = 'TechChipsDB' 
-        username = 'techchips_admin' 
-        password = '>mxvz)tx82xvYWU'
+        server = "tcp:techchipsdbserver1.database.windows.net"
+        database = "TechChipsDB"
+        username = "techchips_admin"
+        password = ">mxvz)tx82xvYWU"
 
-        if os.name == 'nt':
-            cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+        if os.name == "nt":
+            cnxn = pyodbc.connect(
+                "DRIVER={SQL Server};SERVER="
+                + server
+                + ";DATABASE="
+                + database
+                + ";ENCRYPT=yes;UID="
+                + username
+                + ";PWD="
+                + password
+            )
         else:
-            cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+            cnxn = pyodbc.connect(
+                "DRIVER={ODBC Driver 18 for SQL Server};SERVER="
+                + server
+                + ";DATABASE="
+                + database
+                + ";ENCRYPT=yes;UID="
+                + username
+                + ";PWD="
+                + password
+            )
 
         cursor = cnxn.cursor()
-        cursor.execute('select * from app.product where productID = {}'.format(productID))
+        cursor.execute(
+            "select * from app.product where productID = {}".format(productID)
+        )
         while 1:
             row = cursor.fetchone()
             if not row:
@@ -39,6 +61,8 @@ def product_page():
             productPrice = row[2]
         cnxn.close()
 
-        return 'The Product ID is {}. The Product Name is {}. The Product Price is {}'.format(productID, productName, productPrice)
+        return "The Product ID is {}. The Product Name is {}. The Product Price is {}".format(
+            productID, productName, productPrice
+        )
     except Exception as e:
-        print (e)
+        print(e)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from shark-attack!")
+
+
+if __name__ == "__main__":
+    main()
